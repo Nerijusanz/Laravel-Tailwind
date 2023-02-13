752d936682b9717247bbf9e44e59283ffa3bb2cb
Nerijus <nerijusanz@gmail.com>
2023-02-13 17:59:12 +0200

    fix . added app.js link into head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <h1 class="text-3xl font-bold underline bg-red-700">
